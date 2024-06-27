5ba043f76a4a731828df0e3315c8cbf180b0c529
brain0071 <18341314091brain@gmail.com>
2024-06-27 10:05:51 +0800

    20240627

---
+++ scripts/ekf_class.py
@@ -0,0 +1,167 @@
+import numpy as np
+
+
+class ExtendedKalmanFilter(object):
+    def __init__(self, x0=[1.0, 1.0, 0.5]):
+
+        """ initialize EKF """
+        self.__x_est_0 = np.array([[x0[0]], [x0[1]], [x0[2]]]).reshape((3, 1))
+        self.__x_est = self.__x_est_0
+        # standard deviations
+        self.__sig_x1 = 0.200
+        self.__sig_x2 = 0.200
+        self.__sig_x3 = 0.100
+
+        self.__p_mat_0 = np.array(np.diag([self.__sig_x1 ** 2,
+                                           self.__sig_x2 ** 2,
+                                           self.__sig_x3 ** 2]))
+        self.__p_mat = self.__p_mat_0
+        # process noise
+        self.__sig_w1 = 0.100
+        self.__sig_w2 = 0.100
+        self.__sig_w3 = 0.100
+        self.__q_mat = np.array(np.diag([self.__sig_w1 ** 2,
+                                         self.__sig_w2 ** 2,
+                                         self.__sig_w3 ** 2]))
+
+        # measurement noise
+        # --> see measurement_covariance_model
+        self.__sig_r = 1
+        self.__r_mat = self.__sig_r ** 2
+        self.__max_dist_to_tag = 3
+
+        # initial values and system dynamic (=eye)
+        self.__i_mat = np.eye(3)
+
+        # self.__z_meas = np.zeros(self.__max_tag)
+        # self.__y_est = np.zeros(self.__max_tag)
+        # self.__r_dist = np.zeros(self.__max_tag)
+
+    def set_x_0(self, x0):
+        self.__x_est = x0
+        return True
+
+    def set_p_mat_0(self, p0):
+        self.__p_mat = p0
+        return True
+
+    def reset_ekf(self):
+        self.__x_est = self.__x_est_0
+        self.__p_mat = self.__p_mat_0
+
+    def get_x_est(self):
+        return self.__x_est.round(decimals=3)
+
+    def get_p_mat(self):
+        return self.__p_mat
+
+    def get_z_meas(self):
+        return self.__z_meas
+
+    def get_y_est(self):
+        return self.__y_est
+
+    # measurement function
+    def h(self, x, vis_tags):
+        num_vis_tags = vis_tags.shape[0]
+        z = np.zeros((num_vis_tags, 1))
+
+        for i, tag in enumerate(vis_tags):
+            tag_pos = tag[1:4]
+            # print("tag pos + " + str(tag_pos))
+            # print("x= " + str(x))
+            # r = sqrt((x - x_tag) ^ 2 + (y - y_tag) ^ 2 + (z - z_tag) ^ 2)
+            r_dist = np.sqrt((x[0] - tag_pos[0]) ** 2 +
+                             (x[1] - tag_pos[1]) ** 2 +
+                             (x[2] - tag_pos[2]) ** 2)
+            # print ("r = " + str(r_dist))
+            z[i, 0] = r_dist
+
+        return z
+
+    # Jacobian of the measurement function
+    def h_jacobian(self, x, vis_tags):
+
+        num_vis_tags = vis_tags.shape[0]
+        h_jac = np.zeros((num_vis_tags, 3))
+
+        for i, tag in enumerate(vis_tags):
+            tag_pos = tag[1:4]
+            # r = sqrt((x - x_tag) ^ 2 + (y - y_tag) ^ 2 + (z - z_tag) ^ 2)
+            r_dist = np.sqrt((x[0] - tag_pos[0]) ** 2 +
+                             (x[1] - tag_pos[1]) ** 2 +
+                             (x[2] - tag_pos[2]) ** 2)
+
+            # dh /dx1
+            h_jac_x1 = 0.5 * 2.0 * (x[0] - tag_pos[0]) / r_dist
+            # dh /dx2
+            h_jac_x2 = 0.5 * 2.0 * (x[1] - tag_pos[1]) / r_dist
+            # dh /dx3
+            h_jac_x3 = 0.5 * 2.0 * (x[2] - tag_pos[2]) / r_dist
+
+            h_jac[i, 0:3] = [h_jac_x1, h_jac_x2, h_jac_x3]
+
+        return h_jac  # dim [num_tag X 3]
+
+    def prediction(self):
+        """ prediction """
+        self.__x_est = self.__x_est  # + np.random.randn(3, 1) * 1  # = I * x_est
+        self.__p_mat = self.__i_mat.dot(self.__p_mat.dot(self.__i_mat)) + self.__q_mat
+        return True
+
+    def update(self, z_meas_tags):
+        """ innovation """
+        num_meas = z_meas_tags.shape[0]
+        # get new measurement
+        z_meas = z_meas_tags[:, 0].reshape(num_meas, 1)
+
+        # estimate measurement from x_est
+        z_est = self.h(self.__x_est, z_meas_tags)
+        z_tild = z_meas - z_est
+
+        # calc K-gain
+        h_jac_mat = self.h_jacobian(self.__x_est, z_meas_tags)
+
+        k_mat = np.zeros((3, num_meas))
+        r_mat_temp = np.eye(num_meas) * self.__r_mat  # same measurement noise for all measurements, for the moment
+
+        s_mat = np.dot(h_jac_mat, np.dot(self.__p_mat, h_jac_mat.transpose())) + r_mat_temp
+        s_diag = np.diag(s_mat)
+        # compute k_mat in an interative way
+        for i_tag in range(num_meas):
+            k_mat[:, i_tag] = np.dot(self.__p_mat, h_jac_mat[i_tag, :].transpose()) / s_diag[
+                i_tag]  # 1/s scalar since s_mat is dim = 1x1
+
+        # check distance to tag and reject far away tags
+        b_tag_in_range = z_meas <= self.__max_dist_to_tag
+        # print("bevore estimation")
+        # print(self.__x_est)
+        # print(z_meas)
+        # print(z_est)
+        # self.__x_est = self.__x_est + np.dot(k_mat[:, b_tag_in_range], z_tild[b_tag_in_range,0])  # = x_est + k * y_tild
+        # print(k_mat[:, b_tag_in_range[:, 0]])
+        self.__x_est = self.__x_est + np.matmul(k_mat[:, b_tag_in_range[:, 0]], z_tild[b_tag_in_range]).reshape(
+            (3, 1))  # = x_est + k * y_tild
+        # print("after estimation")
+        # print(self.__x_est)
+        self.__p_mat = np.matmul(
+            (self.__i_mat - np.matmul(k_mat[:, b_tag_in_range[:, 0]], h_jac_mat[b_tag_in_range[:, 0], :])),
+            self.__p_mat)  # = (I-KH)*P
+        
+        # 
+        if self.__x_est[0] > 7 or np.isnan(self.__x_est[0]) or self.__x_est[0] < -1:
+            print("EKF Error!\n")
+            self.__x_est[0] = 1.5
+            self.__p_mat[0] = self.__p_mat_0[0]
+            
+        
+        if self.__x_est[1] > 5 or np.isnan(self.__x_est[1]) or self.__x_est[1] < -1:
+            print("EKF Error!\n")
+            self.__x_est[1] = 1
+            self.__p_mat[1] = self.__p_mat_0[1]
+
+        if self.__x_est[2] > 2 or np.isnan(self.__x_est[2]) or self.__x_est[2] < -1:
+            print("EKF Error!\n")
+            self.__x_est[2] = 0.5
+            self.__p_mat[2] = self.__p_mat_0[2]
+        return True
--- scripts/tank_node.py
+++ scripts/tank_node.py
@@ -11,6 +11,7 @@ from tank_odometry.msg import AprilTagDetectionArray
 from tank_odometry.msg import AprilTagDetection
 
 from numpy import genfromtxt
+import ekf_class
 
 
 # use EKF
@@ -22,84 +23,55 @@ class TANK_Odometry():
     def __init__(self):
         
         rospack = rospkg.RosPack()
-        
         data_path = rospack.get_path("tank_odometry") + '/scripts/calibration_tank.csv'  # in real tank
         self.tags = genfromtxt(data_path, delimiter=',')  # home PC
         self.tags = self.tags[:, 0:4]
+        self.state_dim = 3
+        self.cov_mat = 0.05
         
-        self.tank_pos_pub = rospy.Publisher('tank_pos_vel', Odometry, queue_size=1)
+        self.ekf = ekf_class.ExtendedKalmanFilter()
         
-        # (Xsens)update attitude and rotation 
-        rospy.Subscriber("/auto_grasp/filter/quaternion",QuaternionStamped,self.updateAtt_callback,
-                         queue_size=1)           
-        
-        rospy.Subscriber("/apriltag_pose_array", AprilTagDetectionArray, self.estimate_callback, 
+        self.tank_pos_pub = rospy.Publisher('tank_pos_vel', Odometry, queue_size=1)    
+        rospy.Subscriber("/apriltag_pose_array", AprilTagDetectionArray, self.estimate_callback,
                          queue_size=1)
         
-        self.rotation = np.eye(3)
-        self.att = np.array([1., 0., 0., 0.])  # Quaternion format: qw, qx, qy, qz
-
         self.last_pos_tank = np.array([0, 0, 0])
         self.last_time = time.time()
-
         rospy.spin()
 
-    # Quaternion to rotation
-    def q_to_rot_mat(self):
-        qw, qx, qy, qz = self.att[0], self.att[1], self.att[2], self.att[3]
-        rot_mat = np.array([
-            [1 - 2 * (qy ** 2 + qz ** 2), 2 * (qx * qy - qw * qz), 2 * (qx * qz + qw * qy)],
-            [2 * (qx * qy + qw * qz), 1 - 2 * (qx ** 2 + qz ** 2), 2 * (qy * qz - qw * qx)],
-            [2 * (qx * qz - qw * qy), 2 * (qy * qz + qw * qx), 1 - 2 * (qx ** 2 + qy ** 2)]])
-        return rot_mat
-
-    # update current attitude and rotation matrix
-    def updateAtt_callback(self, msg):
-        q_enu = [msg.quaternion.w,  msg.quaternion.x,  msg.quaternion.y,  msg.quaternion.z]
-        self.att = q_enu
-        self.rotation = self.q_to_rot_mat(self.att)
-
-    
-    # estimate position in tank Coordinate system, and velocity in body Coordinate system 
+    # estimate position and velocity in tank Coordinate system
     # NOTE: tank Coordinate system coincides with xsens Coordinate system
     def estimate_callback(self, msg):
-
-        global x, y, z
-        x_total = []
-        y_total = []
-        z_total = []
-
-        for i, tag in enumerate(msg.detections):
-                        
-            tag_id = int(tag.id[0])
-            index = np.where(self.tags[:, 0] == tag_id)
-
-            # x:forward y:left  z:up
-            y_body = tag.pos.x
-            x_body = tag.pos.y
-            z_body = tag.pos.z
-
-            # NOTE: t265 camera Coordinate system 
-            # https://dev.intelrealsense.com/docs/intel-realsensetm-visual-slam-and-the-t265-tracking-camera
-            # Xsens Coordinate system  
-            # https://mtidocs.movella.com/getting-started-2
-            
-            # NOTE: From t265 camera Coordinate system  transform to tank Coordinate system (Xsens frame)        
-            pos_tmp = np.dot(self.rotation, [x_body, y_body, z_body])  
-            
-            x_total.append(self.tags[index, 1] + pos_tmp[0]) 
-            y_total.append(self.tags[index, 2] + pos_tmp[1]) 
-            z_total.append(self.tags[index, 3] + pos_tmp[2])  
-
-            # rospy.loginfo("Detected Apriltags: %d, %f, %f, %f", 
-                        # tag_id, self.tags[index, 1], self.tags[index, 2], self.tags[index, 3])
         
         current_time = time.time()
+        self.ekf.prediction()
+        # get length of message
+        num_meas = len(msg.detections)
+        # if new measurement: update particles
+        if num_meas >= 1:
+            # (num_meas, 4)
+            measurements = np.zeros((num_meas, 1 + self.state_dim))
+
+
+        for i, tag in enumerate(msg.detections):
+            tag_id = int(tag.id[0])
+            tag_distance_cam = np.array(([tag.pose.pose.pose.position.x * 1.05,
+                                          tag.pose.pose.pose.position.y * 1.1,
+                                          tag.pose.pose.pose.position.z]))
+            measurements[i, 0] = np.linalg.norm(tag_distance_cam)
+            
+            
+            index = np.where(self.tags[:, 0] == tag_id)
+
+            measurements[i, 1:4] = self.tags[index, 1:4]
         
-        x = sum(x_total) / len(x_total)
-        y = sum(y_total) / len(y_total)
-        z = sum(z_total) / len(z_total)
+        self.ekf.update(measurements)
+        estimated_position = self.ekf.get_x_est()
         
+        x = estimated_position[0]
+        y = estimated_position[1]
+        z = estimated_position[2]    
+    
         # get velocity in body Coordinate system (update 5hz)
         t = current_time - self.last_time
         
